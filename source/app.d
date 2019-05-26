8f1336b39c97567bdb9c3985a3f3c35871590e08
Paul Crane <paul@acheronsoft.com>
2019-05-26 02:05:05 -0500

    Rework command handling.

--- source/app.d
+++ source/app.d
@@ -6,18 +6,19 @@ void main(string[] arguments)
 	auto scriptSystem_ = new ScriptSystem;
 	auto commands = arguments[1..$];
 
-	writeln("Length = ", commands.length);
-
-	if(commands.length == 1)
+	if(commands.length >= 1)
 	{
-		writeln(commands[1]);
-	}
-	else
-	{
-		scriptSystem_.addAdditionalCommands(commands[0..$]);
-		// TODO: Pass the additional commands to the script.
-	}
+		writeln("Command passed: ", commands[0]);
 
+		if(commands.length > 1)
+		{
+			scriptSystem_.addAdditionalCommands(commands[1..$]);
+		}
+		else
+		{
+			// No arguments passed after command.
+		}
+	}
 
 	scriptSystem_.loadScripts();
 	debug scriptSystem_.dumpCommands();
