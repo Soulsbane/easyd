caa4335a49650d986ab7d1171942bab7b693f9ae
Paul Crane <paul@acheronsoft.com>
2019-07-06 00:48:37 -0500

    Make getDubVersion return the actual version installed.

--- source/api.d
+++ source/api.d
@@ -12,7 +12,12 @@ mixin template APIFunctions()
 
 	auto getDubVersion()
 	{
-		return "1.15";
+		import std.process : execute;
+		import std.array : split;
+		import std.algorithm : findSplitBefore;
+
+		auto dubOutput = execute(["dub", "--version"]);
+		return dubOutput.output.findSplitBefore(",")[0].split[2];
 	}
 
 	bool isInstalled()
